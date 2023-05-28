52891229fa7f62ea60fc7b2f4c20f5f51a7fcd80
praveenkumar <praveendhara555@gmail.com>
2023-05-28 16:12:50 +0000

    import and exports both

---
+++ index.js
@@ -0,0 +1,3 @@
+let add = require("./greeting/index");
+let string = "Hello Rahul! " + add;
+module.exports = string;
---
+++ message/index.js
@@ -0,0 +1,4 @@
+let greetings = require("../greeting/index");
+let string = `Hello Rahul! ${greetings}`;
+
+module.exports = string;
